3acaf37fe70047d1974e43dd5aba9409d035d9af
mariemxX <mariem.benarfi1@gmail.com>
2019-11-28 18:55:33 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import Details from './components/Details';
+import Default from './components/Default';
+import Modal from './components/Modal';
+import { storeProducts } from './data';
+
+
+function App() {
+  return (             //.container .row  .col-6  i.fas fa-home
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+      <Modal />
+    </React.Fragment>
+  );
+}
+
+export default App;
